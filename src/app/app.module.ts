cc10c96be04fbd0195614a119ef7339a6d1b2f04
mahdiheidari <mahdi.hedary2@gmail.com>
2020-03-28 18:55:45 +0430

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesCompnet } from './courses.componet';
+import { CoursesComponent } from './courses/courses.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesCompnet,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/courses.componet.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector:'courses',
+template:`
+<h2>{{title}}</h2>
+<ul>
+<li *ngFor="let course of courses">
+
+{{course}}
+</li>
+</ul>
+`
+
+})
+export class CoursesCompnet{
+title="salllam";
+courses=[" a","b "," v"];
+}
